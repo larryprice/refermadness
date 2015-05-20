a3ddc97cc1cf315fba001ec9072dcdaf981e96b5
Larry Price <larry.price.dev@gmail.com>
2015-05-19 22:45:41 -0400

    starting login work

--- public/scripts/common.jsx
+++ public/scripts/common.jsx
@@ -41,32 +41,60 @@ var TitleArea = React.createClass({
 });
 
 var LoginButton = React.createClass({
+  togglePanel: function() {
+    if (window.location.pathname === "/") {
+      $(".title").toggleClass("shrink fast");
+    }
+    $("#authenticate-panel").collapse('toggle');
+  },
   render: function() {
     return (
       <div className="col-xs-12 col-sm-2 text-center">
         <button className="login-btn btn btn-default" onclick="javascript:void(0)" data-toggle="collapse"
-                data-target="#authenticate-panel" aria-expanded="false" aria-controls="authenticate-panel">
+                onClick={this.togglePanel} aria-expanded="false" aria-controls="authenticate-panel">
           <span className="glyphicon glyphicon-lock"></span>
           Sign Up or Log In
         </button>
-
-        <div className="collapse" id="authenticate-panel">
-          <div className="well">Hey there, Sailor</div>
-        </div>
       </div>
     )
   }
-})
+});
+
+var AuthenticatePanel = React.createClass({
+  authenticate: function() {
+    console.log("DEAL WITH THIS");
+  },
+  render: function() {
+    return (
+      <div className="row collapse" id="authenticate-panel">
+        <div className="col-xs-12 text-center">
+          <h2><strong>Let&apos;s get you authenticated.</strong><span className="glyphicon glyphicon-question-sign"></span></h2>
+          <div className="g-signin2" data-onsuccess={this.authenticate}></div>
+          <h5>By signing in using the link above, you agree to the <a href="/legal">Terms and Conditions</a>.</h5>
+          <div className="container">
+            <div className="login-faq-question"><strong>Why should I?</strong></div>
+            <div className="login-faq-answer">Authentication helps prevent malicious users from submitting bad or duplicate referral codes and prevents robots from taking over the site.</div>
+            <div className="login-faq-question"><strong>Why Google?</strong></div>
+            <div className="login-faq-answer">Google has a respectable history of protecting user passwords. Authenticating with Google means that Refer Madness will never see your password. It also means one less password for you to remember (and, eventually, forget).</div>
+            <div className="login-faq-question"><strong>Where&apos;s the legal information?</strong></div>
+            <div className="login-faq-answer">You can view the privacy policy and terms of service on <a href="/legal">the legal page</a>.</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
 
 var Header = React.createClass({
   render: function() {
     return (
       <div className="header">
         <div className="container-fluid">
-          <div className="row ">
+          <div className="row">
             <TitleArea smallTitle={this.props.smallTitle} />
             <LoginButton />
           </div>
+          <AuthenticatePanel />
       </div>
     </div>
     )
