465a6d47309732c032803501de0cdc65b9bb732a
Larry Price <larry.price.dev@gmail.com>
2015-05-26 20:13:10 -0400

    adding some structure to the account page

--- public/scripts/account.js
+++ public/scripts/account.js
@@ -1,11 +1,58 @@
+var UserReferralCodes = React.createClass({displayName: "UserReferralCodes",
+  render: function() {
+    return (
+      React.createElement("div", {className: "user-referral-codes container"}
+      )
+    );
+  }
+});
+
+var LoginSettings = React.createClass({displayName: "LoginSettings",
+  getInitialState: function() {
+    return {
+      username: this.props.username
+    }
+  },
+  switchAccounts: function() {
+    console.log("Warning about redirect with confirmation");
+  },
+  deleteAccount: function() {
+    console.log("Warning about deletion with verification textbox");
+  },
+  render: function() {
+    return (
+      React.createElement("div", {className: "login-settings container"}, 
+        React.createElement("h2", {className: "text-center"}, 
+          "You are currently logged in as ", React.createElement("strong", null, this.state.username)
+        ), 
+        React.createElement("div", {className: "row"}, 
+          React.createElement("div", {className: "col-xs-12 text-center"}, 
+            React.createElement("button", {className: "btn btn-default btn-lg switch-accounts"}, 
+            React.createElement("span", {className: "glyphicon glyphicon-transfer"}), 
+              "Use Different Account"
+            )
+          )
+        ), 
+        React.createElement("div", {className: "row"}, 
+          React.createElement("div", {className: "col-xs-12 text-center"}, 
+            React.createElement("button", {className: "btn btn-danger btn-lg delete-account"}, 
+              React.createElement("span", {className: "glyphicon glyphicon-fire"}), 
+              "Delete Refer Madness Account"
+            )
+          )
+        )
+      )
+    );
+  }
+});
+
 var AccountPage = React.createClass({displayName: "AccountPage",
   render: function() {
     return (
       React.createElement("div", {className: "account-home"}, 
         React.createElement(Header, {smallTitle: true}), 
-        React.createElement("div", null, 
-          "SETTINGS"
-        )
+        React.createElement(LoginSettings, {username: "larry.price.dev"}), 
+        React.createElement(UserReferralCodes, null)
       )
     );
   }
--- public/scripts/account.jsx
+++ public/scripts/account.jsx
@@ -1,11 +1,58 @@
+var UserReferralCodes = React.createClass({
+  render: function() {
+    return (
+      <div className="user-referral-codes container">
+      </div>
+    );
+  }
+});
+
+var LoginSettings = React.createClass({
+  getInitialState: function() {
+    return {
+      username: this.props.username
+    }
+  },
+  switchAccounts: function() {
+    console.log("Warning about redirect with confirmation");
+  },
+  deleteAccount: function() {
+    console.log("Warning about deletion with verification textbox");
+  },
+  render: function() {
+    return (
+      <div className="login-settings container">
+        <h2 className="text-center">
+          You are currently logged in as <strong>{this.state.username}</strong>
+        </h2>
+        <div className="row">
+          <div className="col-xs-12 text-center">
+            <button className="btn btn-default btn-lg switch-accounts">
+            <span className="glyphicon glyphicon-transfer"></span>
+              Use Different Account
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 text-center">
+            <button className="btn btn-danger btn-lg delete-account">
+              <span className="glyphicon glyphicon-fire"></span>
+              Delete Refer Madness Account
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
 var AccountPage = React.createClass({
   render: function() {
     return (
       <div className="account-home">
         <Header smallTitle={true} />
-        <div>
-          SETTINGS
-        </div>
+        <LoginSettings username={"larry.price.dev"} />
+        <UserReferralCodes/>
       </div>
     );
   }
