94baa689bd4021731c78f5adfd02f1e1f5417cc9
Larry Price <larry.price.dev@gmail.com>
2015-05-26 22:17:19 -0400

    delete account messages

--- public/scripts/account.js
+++ public/scripts/account.js
@@ -7,17 +7,131 @@ var UserReferralCodes = React.createClass({displayName: "UserReferralCodes",
   }
 });
 
+var SwitchAccounts = React.createClass({displayName: "SwitchAccounts",
+  switchAccounts: function() {
+    console.log("Warning about redirect with confirmation");
+  },
+  render: function () {
+    return (
+      React.createElement("div", {className: "row"}, 
+        React.createElement("div", {className: "col-xs-12 text-center"}, 
+          React.createElement("button", {className: "btn btn-default btn-lg switch-accounts", onClick: this.switchAccounts}, 
+          React.createElement("span", {className: "glyphicon glyphicon-transfer"}), 
+            "Use Different Account"
+          )
+        )
+      )
+    );
+  }
+});
+
+var CancelAccountDeletion = React.createClass({displayName: "CancelAccountDeletion",
+  render: function() {
+    return (
+      React.createElement("button", {className: "btn btn-default btn-lg cancel-account-deletion", onClick: this.props.onClick}, 
+        React.createElement("span", {className: "glyphicon glyphicon glyphicon-ban-circle"}), 
+        "Cancel"
+      )
+    );
+  }
+});
+
+var VerifyAccountDeletionDesparation = React.createClass({displayName: "VerifyAccountDeletionDesparation",
+  render: function() {
+    return (
+      React.createElement("div", {className: "desperate-delete-message collapse text-center"}, 
+        React.createElement("h3", null, 
+          "Wait! Don't go! I never got the chance to tell you, but... ", React.createElement("strong", null, "I love you!")
+        ), 
+        React.createElement("button", {className: "btn btn-danger btn-lg", onClick: this.props.onContinue}, 
+          React.createElement("span", {className: "glyphicon glyphicon-heart-empty"}), 
+          "Sorry, pal, but the feeling's not mutual"
+        ), 
+        React.createElement(CancelAccountDeletion, {onClick: this.props.onCancel})
+      )
+    );
+  }
+});
+
+var VerifyAccountDeletionApology = React.createClass({displayName: "VerifyAccountDeletionApology",
+  render: function() {
+    return (
+      React.createElement("div", {className: "apologetic-delete-message collapse text-center"}, 
+        React.createElement("h4", null, 
+          "...Er. Sorry about that. Overreaction on my part! ", React.createElement("em", null, "Please don't tell my supervisor.")
+        ), 
+        React.createElement("button", {className: "btn btn-danger btn-lg", onClick: this.props.onContinue}, 
+          React.createElement("span", {className: "glyphicon glyphicon-thumbs-up"}), 
+          "Sure, I can be discreet, let's get on with this"
+        ), 
+        React.createElement(CancelAccountDeletion, {onClick: this.props.onCancel})
+      )
+    );
+  }
+});
+
+var VerifyAccountDeletionWarning = React.createClass({displayName: "VerifyAccountDeletionWarning",
+  render: function() {
+    return (
+      React.createElement("div", {className: "warning-delete-message collapse text-center"}, 
+        React.createElement("h3", null, 
+          React.createElement("strong", null, "Continuing will ", React.createElement("em", null, "permanantly delete"), " your account and remove your codes from the system.")
+        ), 
+        React.createElement("h3", null, 
+          "If you really want to leave, please ", React.createElement("strong", null, "enter your Google username in the textbox below"), "."
+        ), 
+        React.createElement("button", {className: "btn btn-danger btn-lg", onClick: this.props.onContinue}, 
+          React.createElement("span", {className: "glyphicon glyphicon-fire"}), 
+          "Delete"
+        ), 
+        React.createElement(CancelAccountDeletion, {onClick: this.props.onCancel})
+      )
+    );
+  }
+});
+
+var DeleteAccount = React.createClass({displayName: "DeleteAccount",
+  initiate: function() {
+    $(".delete-account").addClass("fade-out");
+    $(".desperate-delete-message").collapse("show");
+  },
+  apologize: function() {
+    $(".desperate-delete-message").collapse("hide");
+    $(".apologetic-delete-message").collapse("show");
+  },
+  finalWarning: function() {
+    $(".apologetic-delete-message").collapse("hide");
+    $(".warning-delete-message").collapse("show");
+  },
+  confirmDelete: function() {
+    console.log("send oauth delete request to google, delete account data in our database, clear session");
+  },
+  rejectDelete: function() {
+    $(".desperate-delete-message, .apologetic-delete-message, .warning-delete-message").collapse("hide");
+    $(".delete-account").removeClass("fade-out");
+  },
+  render: function () {
+    return (
+      React.createElement("div", {className: "row delete-account-section"}, 
+        React.createElement("div", {className: "col-xs-12 text-center"}, 
+          React.createElement("button", {className: "btn btn-danger btn-lg delete-account", onClick: this.initiate}, 
+            React.createElement("span", {className: "glyphicon glyphicon-fire"}), 
+            "Delete Refer Madness Account"
+          )
+        ), 
+        React.createElement(VerifyAccountDeletionDesparation, {onContinue: this.apologize, onCancel: this.rejectDelete}), 
+        React.createElement(VerifyAccountDeletionApology, {onContinue: this.finalWarning, onCancel: this.rejectDelete}), 
+        React.createElement(VerifyAccountDeletionWarning, {onContinue: this.confirmDelete, onCancel: this.rejectDelete})
+      )
+    );
+  }
+});
+
 var LoginSettings = React.createClass({displayName: "LoginSettings",
   getInitialState: function() {
     return {
       username: this.props.username
     }
-  },
-  switchAccounts: function() {
-    console.log("Warning about redirect with confirmation");
-  },
-  deleteAccount: function() {
-    console.log("Warning about deletion with verification textbox");
   },
   render: function() {
     return (
@@ -25,22 +139,8 @@ var LoginSettings = React.createClass({displayName: "LoginSettings",
         React.createElement("h2", {className: "text-center"}, 
           "You are currently logged in as ", React.createElement("strong", null, this.state.username)
         ), 
-        React.createElement("div", {className: "row"}, 
-          React.createElement("div", {className: "col-xs-12 text-center"}, 
-            React.createElement("button", {className: "btn btn-default btn-lg switch-accounts"}, 
-            React.createElement("span", {className: "glyphicon glyphicon-transfer"}), 
-              "Use Different Account"
-            )
-          )
-        ), 
-        React.createElement("div", {className: "row"}, 
-          React.createElement("div", {className: "col-xs-12 text-center"}, 
-            React.createElement("button", {className: "btn btn-danger btn-lg delete-account"}, 
-              React.createElement("span", {className: "glyphicon glyphicon-fire"}), 
-              "Delete Refer Madness Account"
-            )
-          )
-        )
+        React.createElement(SwitchAccounts, null), 
+        React.createElement(DeleteAccount, null)
       )
     );
   }
--- public/scripts/account.jsx
+++ public/scripts/account.jsx
@@ -7,17 +7,131 @@ var UserReferralCodes = React.createClass({
   }
 });
 
+var SwitchAccounts = React.createClass({
+  switchAccounts: function() {
+    console.log("Warning about redirect with confirmation");
+  },
+  render: function () {
+    return (
+      <div className="row">
+        <div className="col-xs-12 text-center">
+          <button className="btn btn-default btn-lg switch-accounts" onClick={this.switchAccounts}>
+          <span className="glyphicon glyphicon-transfer"></span>
+            Use Different Account
+          </button>
+        </div>
+      </div>
+    );
+  }
+});
+
+var CancelAccountDeletion = React.createClass({
+  render: function() {
+    return (
+      <button className="btn btn-default btn-lg cancel-account-deletion" onClick={this.props.onClick}>
+        <span className="glyphicon glyphicon glyphicon-ban-circle"></span>
+        Cancel
+      </button>
+    );
+  }
+});
+
+var VerifyAccountDeletionDesparation = React.createClass({
+  render: function() {
+    return (
+      <div className="desperate-delete-message collapse text-center">
+        <h3>
+          Wait! Don&apos;t go! I never got the chance to tell you, but... <strong>I love you!</strong>
+        </h3>
+        <button className="btn btn-danger btn-lg" onClick={this.props.onContinue}>
+          <span className="glyphicon glyphicon-heart-empty"></span>
+          Sorry, pal, but the feeling&apos;s not mutual
+        </button>
+        <CancelAccountDeletion onClick={this.props.onCancel} />
+      </div>
+    );
+  }
+});
+
+var VerifyAccountDeletionApology = React.createClass({
+  render: function() {
+    return (
+      <div className="apologetic-delete-message collapse text-center">
+        <h4>
+          ...Er. Sorry about that. Overreaction on my part! <em>Please don&apos;t tell my supervisor.</em>
+        </h4>
+        <button className="btn btn-danger btn-lg" onClick={this.props.onContinue}>
+          <span className="glyphicon glyphicon-thumbs-up"></span>
+          Sure, I can be discreet, let&apos;s get on with this
+        </button>
+        <CancelAccountDeletion onClick={this.props.onCancel} />
+      </div>
+    );
+  }
+});
+
+var VerifyAccountDeletionWarning = React.createClass({
+  render: function() {
+    return (
+      <div className="warning-delete-message collapse text-center">
+        <h3>
+          <strong>Continuing will <em>permanantly delete</em> your account and remove your codes from the system.</strong>
+        </h3>
+        <h3>
+          If you really want to leave, please <strong>enter your Google username in the textbox below</strong>.
+        </h3>
+        <button className="btn btn-danger btn-lg" onClick={this.props.onContinue}>
+          <span className="glyphicon glyphicon-fire"></span>
+          Delete
+        </button>
+        <CancelAccountDeletion onClick={this.props.onCancel} />
+      </div>
+    );
+  }
+});
+
+var DeleteAccount = React.createClass({
+  initiate: function() {
+    $(".delete-account").addClass("fade-out");
+    $(".desperate-delete-message").collapse("show");
+  },
+  apologize: function() {
+    $(".desperate-delete-message").collapse("hide");
+    $(".apologetic-delete-message").collapse("show");
+  },
+  finalWarning: function() {
+    $(".apologetic-delete-message").collapse("hide");
+    $(".warning-delete-message").collapse("show");
+  },
+  confirmDelete: function() {
+    console.log("send oauth delete request to google, delete account data in our database, clear session");
+  },
+  rejectDelete: function() {
+    $(".desperate-delete-message, .apologetic-delete-message, .warning-delete-message").collapse("hide");
+    $(".delete-account").removeClass("fade-out");
+  },
+  render: function () {
+    return (
+      <div className="row delete-account-section">
+        <div className="col-xs-12 text-center">
+          <button className="btn btn-danger btn-lg delete-account" onClick={this.initiate}>
+            <span className="glyphicon glyphicon-fire"></span>
+            Delete Refer Madness Account
+          </button>
+        </div>
+        <VerifyAccountDeletionDesparation onContinue={this.apologize} onCancel={this.rejectDelete} />
+        <VerifyAccountDeletionApology onContinue={this.finalWarning} onCancel={this.rejectDelete} />
+        <VerifyAccountDeletionWarning onContinue={this.confirmDelete} onCancel={this.rejectDelete} />
+      </div>
+    );
+  }
+});
+
 var LoginSettings = React.createClass({
   getInitialState: function() {
     return {
       username: this.props.username
     }
-  },
-  switchAccounts: function() {
-    console.log("Warning about redirect with confirmation");
-  },
-  deleteAccount: function() {
-    console.log("Warning about deletion with verification textbox");
   },
   render: function() {
     return (
@@ -25,22 +139,8 @@ var LoginSettings = React.createClass({
         <h2 className="text-center">
           You are currently logged in as <strong>{this.state.username}</strong>
         </h2>
-        <div className="row">
-          <div className="col-xs-12 text-center">
-            <button className="btn btn-default btn-lg switch-accounts">
-            <span className="glyphicon glyphicon-transfer"></span>
-              Use Different Account
-            </button>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-12 text-center">
-            <button className="btn btn-danger btn-lg delete-account">
-              <span className="glyphicon glyphicon-fire"></span>
-              Delete Refer Madness Account
-            </button>
-          </div>
-        </div>
+        <SwitchAccounts />
+        <DeleteAccount />
       </div>
     );
   }
