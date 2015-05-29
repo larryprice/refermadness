eec0bde6ed8e5042d712d6b890042a8cbe9dc38e
Larry Price <larry.price.dev@gmail.com>
2015-05-28 21:15:29 -0400

    adding controllers directory

--- main.go
+++ main.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
+	"github.com/larryprice/refermadness/controllers"
 	"github.com/unrolled/render"
 	"html/template"
 	"net/http"
-	"net/url"
 	"os"
-	"encoding/json"
-	"io/ioutil"
-	"github.com/dgrijalva/jwt-go"
 )
-
-type service struct {
-	Name        string
-	ID          string
-	URL         string
-	Description string
-}
 
 func main() {
 	n := negroni.Classic()
@@ -48,44 +37,7 @@ func main() {
 		t, _ := template.ParseFiles("views/layout.html", "views/service.html")
 		t.Execute(w, nil)
 	})
-	router.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		redirectURI := r.Host
-		if r.URL.Scheme == "" {
-			redirectURI = "http%3A%2F%2F" + redirectURI + "%2foauth2callback"
-		} else {
-			redirectURI = "https%3A%2F%2F" + redirectURI + "%2foauth2callback"
-		}
-		http.Redirect(w, r,
-			"https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri="+
-				redirectURI+"&response_type=code&client_id="+os.Getenv("GOOGLE_OAUTH2_CLIENT_ID"),
-			http.StatusTemporaryRedirect)
-	})
-	router.HandleFunc("/oauth2callback", func(w http.ResponseWriter, r *http.Request) {
-		if r.FormValue("error") != "" {
-			fmt.Println("Error in OAuth", r.FormValue("error"))
-			// redirect to wherever we came from with error message
-		}
-		redirectURI := r.Host
-		if r.URL.Scheme == "" {
-			redirectURI = "http://" + redirectURI + "/oauth2callback"
-		} else {
-			redirectURI = "https://" + redirectURI + "/oauth2callback"
-		}
-		// send token request
-		resp, err := http.PostForm("https://www.googleapis.com/oauth2/v3/token",
-			url.Values{"code": {r.FormValue("code")}, "grant_type": {"authorization_code"}, "redirect_uri": {redirectURI},
-			"client_id": {os.Getenv("GOOGLE_OAUTH2_CLIENT_ID")}, "client_secret": {os.Getenv("GOOGLE_OAUTH2_CLIENT_SECRET")}})
-
-		var result map[string]interface{}
-		body, err := ioutil.ReadAll(resp.Body)
-		json.Unmarshal(body, &result)
-		token, _ := jwt.Parse(result["id_token"].(string), func(token *jwt.Token) (interface{}, error) {
-      return result["access_token"], nil
-    })
-    fmt.Println(token.Claims["email"], err, resp.StatusCode)
-
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-	})
+	controllers.Create(router)
 
 	r := render.New()
 
