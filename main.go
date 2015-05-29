b488287d45509657719c38a6b5c857e9ff1cfa12
Larry Price <larry.price.dev@gmail.com>
2015-05-29 07:51:36 -0400

    moving most work out of main

--- main.go
+++ main.go
@@ -1,51 +1,19 @@
 package main
 
 import (
-	"github.com/codegangsta/negroni"
-	"github.com/gorilla/mux"
-	"github.com/larryprice/refermadness/controllers"
-	"html/template"
-	"net/http"
+	"github.com/larryprice/refermadness/web"
+	"github.com/stretchr/graceful"
 	"os"
 )
 
 func main() {
-	n := negroni.Classic()
-	router := mux.NewRouter()
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t, _ := template.ParseFiles("views/layout.html", "views/index.html")
-		t.Execute(w, nil)
-	})
-	router.HandleFunc("/legal", func(w http.ResponseWriter, r *http.Request) {
-		t, _ := template.ParseFiles("views/layout.html", "views/legal.html")
-		t.Execute(w, nil)
-	})
-	router.HandleFunc("/account", func(w http.ResponseWriter, r *http.Request) {
-		t, _ := template.ParseFiles("views/layout.html", "views/account.html")
-		t.Execute(w, nil)
-	})
-	router.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		t, _ := template.ParseFiles("views/layout.html", "views/search.html")
-		t.Execute(w, nil)
-	})
-	router.HandleFunc("/service/create", func(w http.ResponseWriter, r *http.Request) {
-		t, _ := template.ParseFiles("views/layout.html", "views/create-service.html")
-		t.Execute(w, nil)
-	})
-	router.HandleFunc("/service/{id}", func(w http.ResponseWriter, r *http.Request) {
-		t, _ := template.ParseFiles("views/layout.html", "views/service.html")
-		t.Execute(w, nil)
-	})
-	authenticationController := controllers.NewAuthenticationController(os.Getenv("GOOGLE_OAUTH2_CLIENT_ID"),
-		os.Getenv("GOOGLE_OAUTH2_CLIENT_SECRET"), os.Getenv("ENVIRONMENT") == "development")
-	authenticationController.Register(router)
+  db := web.NewDatabase(os.Getenv("DB_PORT_27017_TCP_ADDR"), os.Getenv("DATABASE_NAME"), 0)
+	s := web.NewServer(*db, os.Getenv("GOOGLE_OAUTH2_CLIENT_ID"), os.Getenv("GOOGLE_OAUTH2_CLIENT_SECRET"),
+                     os.Getenv("ENVIRONMENT") == "development")
 
-	n.UseHandler(router)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
-
-	n.Run(":" + port)
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "3000"
+  }
+	graceful.Run(":"+port, 0, s)
 }
---
+++ utils/session_manager.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+  "github.com/goincremental/negroni-sessions"
+  "net/http"
+)
+
+type SessionManager interface {
+  GetAccessToken(*http.Request) string
+  SetAccessToken(*http.Request, string)
+  DeleteAccessToken(*http.Request)
+}
+
+type SessionManagerImpl struct {
+}
+
+func NewSessionManager() *SessionManagerImpl {
+  return &SessionManagerImpl{}
+}
+
+func (sa *SessionManagerImpl) Get(req *http.Request, key string) string {
+  if val := sessions.GetSession(req).Get(key); val != nil {
+    return val.(string)
+  }
+
+  return ""
+}
+
+func (sa *SessionManagerImpl) Set(req *http.Request, key, value string) {
+  sessions.GetSession(req).Set(key, value)
+}
+
+func (sa *SessionManagerImpl) Delete(req *http.Request, key string) {
+  sessions.GetSession(req).Delete(key)
+}
