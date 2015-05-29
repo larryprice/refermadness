2a929e5a401226f8bc422fb152b557d2efafb9c9
Larry Price <larry.price.dev@gmail.com>
2015-05-29 07:56:53 -0400

    setting dbURL based on environment

--- main.go
+++ main.go
@@ -7,9 +7,14 @@ import (
 )
 
 func main() {
-  db := web.NewDatabase(os.Getenv("DB_PORT_27017_TCP_ADDR"), os.Getenv("DATABASE_NAME"), 0)
-	s := web.NewServer(*db, os.Getenv("GOOGLE_OAUTH2_CLIENT_ID"), os.Getenv("GOOGLE_OAUTH2_CLIENT_SECRET"),
-                     os.Getenv("ENVIRONMENT") == "development")
+  isDevelopment := os.Getenv("ENVIRONMENT") == "development"
+  dbURL := os.Getenv("MONGOLAB_URI")
+  if isDevelopment {
+    dbURL = os.Getenv("DB_PORT_27017_TCP_ADDR")
+  }
+
+  db := web.NewDatabase(dbURL, os.Getenv("DATABASE_NAME"), 0)
+	s := web.NewServer(*db, os.Getenv("GOOGLE_OAUTH2_CLIENT_ID"), os.Getenv("GOOGLE_OAUTH2_CLIENT_SECRET"), isDevelopment)
 
   port := os.Getenv("PORT")
   if port == "" {
