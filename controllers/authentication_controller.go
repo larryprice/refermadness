ec130d3c0e079d2dcb891bfb1e1231e8df7da38f
Larry Price <larry.price.dev@gmail.com>
2015-05-28 21:29:22 -0400

    changing https detection

--- controllers/authentication_controller.go
+++ controllers/authentication_controller.go
@@ -13,15 +13,13 @@ import (
 
 func Create(router *mux.Router) {
 	router.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		redirectURI := r.Host
-		if r.URL.Scheme == "" {
-			redirectURI = "http%3A%2F%2F" + redirectURI + "%2foauth2callback"
-		} else {
-			redirectURI = "https%3A%2F%2F" + redirectURI + "%2foauth2callback"
+		scheme := "http"
+		if os.Getenv("ENVIRONMENT") != "development" {
+			scheme += "s"
 		}
-		http.Redirect(w, r,
-			"https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri="+
-				redirectURI+"&response_type=code&client_id="+os.Getenv("GOOGLE_OAUTH2_CLIENT_ID"),
+
+		http.Redirect(w, r, "https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri="+
+				scheme + "%3A%2F%2F" + r.Host + "%2foauth2callback"+"&response_type=code&client_id="+os.Getenv("GOOGLE_OAUTH2_CLIENT_ID"),
 			http.StatusTemporaryRedirect)
 	})
 	router.HandleFunc("/oauth2callback", func(w http.ResponseWriter, r *http.Request) {
@@ -29,15 +27,13 @@ func Create(router *mux.Router) {
 			fmt.Println("Error in OAuth", r.FormValue("error"))
 			// redirect to wherever we came from with error message
 		}
-		redirectURI := r.Host
-		if r.URL.Scheme == "" {
-			redirectURI = "http://" + redirectURI + "/oauth2callback"
-		} else {
-			redirectURI = "https://" + redirectURI + "/oauth2callback"
+		scheme := "http"
+		if os.Getenv("ENVIRONMENT") != "development" {
+			scheme += "s"
 		}
 		// send token request
 		resp, err := http.PostForm("https://www.googleapis.com/oauth2/v3/token",
-			url.Values{"code": {r.FormValue("code")}, "grant_type": {"authorization_code"}, "redirect_uri": {redirectURI},
+			url.Values{"code": {r.FormValue("code")}, "grant_type": {"authorization_code"}, "redirect_uri": { scheme + "://" + r.Host + "/oauth2callback"},
 				"client_id": {os.Getenv("GOOGLE_OAUTH2_CLIENT_ID")}, "client_secret": {os.Getenv("GOOGLE_OAUTH2_CLIENT_SECRET")}})
 
 		var result map[string]interface{}
